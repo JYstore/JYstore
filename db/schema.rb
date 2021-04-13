fb0a33ba3e9c5b4d374f48e1c5e44d4d7b578d64
zhongjinye <610505041@qq.com>
2021-04-13 18:09:15 +0800

    t119 implement image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_092234) do
+ActiveRecord::Schema.define(version: 2021_04_13_095755) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_04_13_092234) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
