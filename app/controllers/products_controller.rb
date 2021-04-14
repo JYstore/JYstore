e35a998ff204b8a588bb391bc12ecc0c40668d3c
zhongjinye <610505041@qq.com>
2021-04-14 14:50:34 +0800

    t153 fix the bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,7 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    redirect_to :back
+    redirect_to product_path
     flash[:notice] = "测试加入购物车"
   end
 
