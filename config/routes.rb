2126c2f5a779a90d90a9e59ba43c8e916b3ad4c3
zhongjinye <610505041@qq.com>
2021-04-14 15:52:30 +0800

    t160 add delete cart_item and empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
     collection do
       delete :clean
     end
-  end 
+  end
   resources :products do
     member do
       post :add_to_cart
     end
   end
-
+  resources :cart_items
 end
