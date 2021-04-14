ae586a3dfdea4fff3e5bb0754212bd0e539cb724
zhongjinye <610505041@qq.com>
2021-04-14 11:35:17 +0800

    商家可以上架商品

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_095755) do
+ActiveRecord::Schema.define(version: 2021_04_14_032021) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_04_13_095755) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.boolean "is_hidden", default: true
   end
 
   create_table "users", force: :cascade do |t|
