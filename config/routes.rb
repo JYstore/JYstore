6444584d0226f7c9647acc885ed453ddabf5466b
zhongjinye <610505041@qq.com>
2021-04-14 15:19:38 +0800

    t159 implement carts index function

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :carts
   resources :products do
     member do
       post :add_to_cart
