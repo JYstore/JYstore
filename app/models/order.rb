c3b1f77ebee6a8fa77196c4c35b0cfd17f93ee75
zhongjinye <610505041@qq.com>
2021-04-15 10:01:10 +0800

    t171 users can see their orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,9 @@ class Order < ApplicationRecord
 
    validates :billing_name, presence: true
    validates :billing_address, presence: true
+   validates :billing_contact_information, presence: true
    validates :shipping_name, presence: true
    validates :shipping_address, presence: true
+   validates :shipping_contact_information, presence: true
    has_many :product_lists
 end
