decfa85430c7336047db76f8012527416aa235a1
zhongjinye <610505041@qq.com>
2021-04-16 14:31:55 +0800

    t184 delete billing column

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,9 +7,6 @@ class Order < ApplicationRecord
 
    belongs_to :user
 
-   validates :billing_name, presence: true
-   validates :billing_address, presence: true
-   validates :billing_contact_information, presence: true
    validates :shipping_name, presence: true
    validates :shipping_address, presence: true
    validates :shipping_contact_information, presence: true
