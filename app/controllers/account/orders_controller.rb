7b2edd363bcaad8ee3b44ebac4a299c621262d52
zhongjinye <610505041@qq.com>
2021-05-12 17:47:06 +0800

    t261

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -4,4 +4,9 @@ class Account::OrdersController < ApplicationController
    def index
      @orders = current_user.orders.order("id DESC")
    end
+
+   def show
+     @order = Order.find(params[:id])
+     @products = @order.products
+   end
 end
