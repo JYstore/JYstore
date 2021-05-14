c9dc3fe097fbe55118e5f917e7b4502894de2da4
zhongjinye <610505041@qq.com>
2021-05-14 15:56:20 +0800

    个人订单页面初稿

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,7 @@ class OrdersController < ApplicationController
          product_list.product_name = cart_item.product.title
          product_list.product_price = cart_item.product.price
          product_list.quantity = cart_item.quantity
+         product_list.product_image = cart_item.product.image
          product_list.save
        end
        current_cart.clean!
