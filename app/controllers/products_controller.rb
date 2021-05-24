f1d37bc6e8624358ffad3c915c58732d0251359c
zhongjinye <610505041@qq.com>
2021-05-24 20:57:06 +0800

    实现搜索功能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :validate_search_key, only: [:search]
+
   def index
     if params[:category].blank?
       @products = Product.where(:is_hidden => false).paginate(:page => params[:page], :per_page => 8)
@@ -30,6 +32,24 @@ class ProductsController < ApplicationController
       flash[:warning] = "你的购物车内已有此物品"
     end
     redirect_to product_path
+  end
+
+  def search
+    if @query_string.present?
+      search_result = Product.ransack(@search_criteria).result(:distinct => true)
+      @products = search_result.paginate(:page => params[:page], :per_page => 5 )
+    end
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
   end
 
   private
